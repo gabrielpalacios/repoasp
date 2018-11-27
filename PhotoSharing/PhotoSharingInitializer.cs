f286808fd07c0964b8116e591b2414bf9d084d19
gab <palaciosgabriel@hotmail.com>
2018-11-26 21:36:25 -0300

    iniio2

--- PhotoSharing/PhotoSharingInitializer.cs
+++ PhotoSharing/PhotoSharingInitializer.cs
@@ -5,7 +5,18 @@ namespace PhotoSharing
 {
     public class PhotoSharingInitializer :  DropCreateDatabaseAlways<PhotoSharingContext>
         {
-            
-        
+        protected override void Seed(PhotoSharingContext context)
+        {
+            base.Seed(context);
+
+            Photo a1 =  new Photo
+            {
+                Title = "1",
+                CreateDate = System.DateTime.Now
+            };
+            context.Photos.Add(a1);
+            context.SaveChanges();
+        }
+
     }
 }
